f590cd460d0434c16e7de588276a18eefb3649a1
aripzz <79347655+aripzz@users.noreply.github.com>
2025-06-30 12:45:12 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body className="overflow-x-hidden">
         <ThemeProvider attribute="class" defaultTheme="system">
           <SideProvider>
             <main>
