777aecfe4645224bace69ce482aaca70827689d1
aripzz <79347655+aripzz@users.noreply.github.com>
2026-01-06 13:44:56 +0700

    Update ContactSection.tsx

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -32,9 +32,23 @@ const SocialIcons = (): React.ReactElement => (
             aria-label="instagram"
             className="text-[#EEEEEE] hover:text-white"
         >
-            <span className="sr-only">instagram</span>
+            <span className="sr-only">Instagram</span>
             <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">
                 <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+                <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+                <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+            </svg>
+        </a>
+        <a
+            href="https://www.linkedin.com/in/arifn-ab/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="linkedin"
+            className="text-[#EEEEEE] hover:text-white"
+        >
+            <span className="sr-only">LinkedIn</span>
+            <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" aria-hidden="true">
+                <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />
                 <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
                 <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
             </svg>
